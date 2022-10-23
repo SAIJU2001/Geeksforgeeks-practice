6cf85b304e962212481d351c92ffccdbdadf0c70
saikat naskar <102968768+SAIJU2001@users.noreply.github.com>
2022-10-23 09:06:56 +0530

    Add files via upload

---
+++ MaximumNumberOf2X2Squares.java
@@ -0,0 +1,64 @@
+/*                  Maximum number of 2X2 squares
+Given the base (in units) of a right-angled isoceles traingle, find the maximum number of 2X2 squares that can fit in the triangle with given base.
+
+Example 1:
+Input: 8
+Output: 6
+Explanation:
+Please refer below diagram for explanation.
+squares-in-triangle
+
+Example 2:
+Input : 7
+Output : 3
+Explanation : In the base we can keep 2 squares
+and above the two squares we can only keep 1 square.  
+So the total number of squares are equal to 3.
+ 
+Your task :
+You don't need to read input or print anything. Your task is to complete the function numberOfSquares() which takes the base of the triangle as input and returns the maximum number of squares that can be fit into the triangle with given base.
+ 
+Expected Time Complexity: O(1)
+Expected Auxiliary Space: O(1)
+ 
+Constraints :
+1 <= N <= 10^9                          */
+
+
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.util.*;
+import java.io.*;
+import java.lang.*;
+
+class MaximumNumberOf2X2Squares
+{
+    public static void main(String args[])throws IOException
+    {
+        BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(read.readLine());//testcases
+        
+        while(t-- > 0)
+        {
+            long n = Long.parseLong(read.readLine());//input n
+            
+            Solution ob= new Solution();
+            System.out.println(ob.numberOfSquares(n));
+        }
+    }
+}
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution
+{
+    public static long numberOfSquares(long base)
+    {
+        //code here
+        long n=base/2;
+        return n*(n-1)/2;
+    }
+}
